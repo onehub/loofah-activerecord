2b2b6379f18af68c0933b13b7d64e3ecfdebe818
Rocco Stanzione <grasshopper@linuxkungfu.org>
2013-12-06 19:09:02 -0600

    Rails 4 compat

--- lib/loofah/activerecord/railtie.rb
+++ lib/loofah/activerecord/railtie.rb
@@ -2,7 +2,10 @@ require 'rails'
 module Loofah::ActiveRecord
   class Railtie < Rails::Railtie
     initializer "loofah-activerecord.initialize" do |app|
-      activerecord_railtie = app.railties.all.select {|railtie| railtie.class.to_s == "ActiveRecord::Railtie" }
+      activerecord_railtie = false
+      app.railties.each do |railtie|
+        activerecord_railtie = true if railtie.class.to_s == "ActiveRecord::Railtie"
+      end
       if activerecord_railtie
         require 'loofah/activerecord/active_record'
         require 'loofah/activerecord/xss_foliate'
